923d0409f9772ed3b3bec25790a0fdae51d2dad8
Seb Bailey <sebastian@clearscore.com>
2020-03-16 17:13:34 +0000

    starts adding web components

--- src/modules/CSWeb.js
+++ src/modules/CSWeb.js
@@ -1,8 +1,136 @@
 const symbols = [
     {
-        componentName: '',
-        names: [''],
+        componentName: "Accordion",
+        names: [
+            "Accordion/01 Header/02 XS-S/03 Text Only/Basic",
+            "Accordion/01 Header/02 XS-S/03 Text Only/Description",
+            "Accordion/01 Header/01 M-XL/03 Text Only/Basic",
+            "Accordion/01 Header/01 M-XL/03 Text Only/2 Columns",
+            "Accordion/01 Header/01 M-XL/03 Text Only/3 Columns",
+            "Accordion/01 Header/02 XS-S/02 Icon/Basic",
+            "Accordion/01 Header/02 XS-S/02 Icon/Description",
+            "Accordion/01 Header/01 M-XL/02 Icon/2 Columns",
+            "Accordion/01 Header/01 M-XL/02 Icon/3 Columns",
+            "Accordion/01 Header/02 XS-S/01 Logo/Basic",
+            "Accordion/01 Header/02 XS-S/01 Logo/Balance",
+            "Accordion/01 Header/01 M-XL/01 Logo/2 Columns",
+            "Accordion/01 Header/01 M-XL/01 Logo/3 Columns"
+        ]
+    },
+    {
+        componentName: "Accordion.ContentSection",
+        names: [
+            "Accordion/02 Content/04 Details/02 XS-S",
+            "Accordion/02 Content/04 Details/01 M-XL"
+        ]
+    },
+    {
+        componentName: "Accordion.ContentAccordion",
+        names: [
+            "Accordion/02 Content/03 Content Accordion/02 XS-S/01 Closed",
+            "Accordion/02 Content/03 Content Accordion/02 XS-S/02 Open",
+            "Accordion/02 Content/03 Content Accordion/01 M-XL/01 Closed",
+            "Accordion/02 Content/03 Content Accordion/01 M-XL/02 Open"
+        ]
+    },
+    {
+        componentName: "Accordion.ContentAccordionList",
+        names: [
+            "Accordion/02 Content/03 Content Accordion/02 XS-S/04 Content Open",
+            "Accordion/02 Content/03 Content Accordion/02 XS-S/03 Content",
+            "Accordion/02 Content/03 Content Accordion/01 M-XL/04 Content Open",
+            "Accordion/02 Content/03 Content Accordion/01 M-XL/03 Content"
+        ]
+    },
+    {
+        componentName: "Autosuggest",
+        names: [
+            "Form/_field/Filter options/__Light/Active",
+            "Form/_field/Filter options/__Light/Dirty",
+            "Form/_field/Filter options/__Light/Loading",
+            "Form/_field/Filter options/_Dark/Active",
+            "Form/_field/Filter options/_Dark/Dirty",
+            "Form/_field/Filter options/_Dark/Loading"
+        ]
+    },
+    {
+        componentName: "Button",
+        names: [
+            "Button/_Tertiary/Small/_Dark/Disabled",
+            "Button/_Tertiary/Large/_Dark/Disabled",
+            "Button/_Tertiary/Small/_Dark/Loading",
+            "Button/_Tertiary/Large/_Dark/Loading",
+            "Button/_Tertiary/Small/_Dark/Hover  Active",
+            "Button/_Tertiary/Large/_Dark/Hover  Active",
+            "Button/_Tertiary/Small/_Dark/_Default",
+            "Button/_Tertiary/Large/_Dark/_Default",
+            "Button/_Secondary/Small/_Dark/Disabled",
+            "Button/_Secondary/Large/_Dark/Disabled",
+            "Button/_Secondary/Small/_Dark/Loading",
+            "Button/_Secondary/Large/_Dark/Loading",
+            "Button/_Secondary/Small/_Dark/Hover  Active",
+            "Button/_Secondary/Large/_Dark/Hover  Active",
+            "Button/_Secondary/Small/_Dark/_Default",
+            "Button/_Secondary/Large/_Dark/_Default",
+            "Button/__Primary/Small/_Dark/Disabled",
+            "Button/__Primary/Large/_Dark/Disabled",
+            "Button/__Primary/Small/_Dark/Loading",
+            "Button/__Primary/Large/_Dark/Loading",
+            "Button/__Primary/Small/_Dark/Hover  Active",
+            "Button/__Primary/Large/_Dark/Hover  Active",
+            "Button/__Primary/Small/_Dark/_Default",
+            "Button/__Primary/Large/_Dark/_Default",
+            "Button/_Tertiary/Small/__Light/Disabled",
+            "Button/_Tertiary/Large/__Light/Disabled",
+            "Button/_Tertiary/Small/__Light/Loading",
+            "Button/_Tertiary/Large/__Light/Loading",
+            "Button/_Tertiary/Small/__Light/Hover  Active",
+            "Button/_Tertiary/Large/__Light/Hover  Active",
+            "Button/_Tertiary/Small/__Light/_Default",
+            "Button/_Tertiary/Large/__Light/_Default",
+            "Button/_Secondary/Small/__Light/Disabled",
+            "Button/_Secondary/Large/__Light/Disabled",
+            "Button/_Secondary/Small/__Light/Loading",
+            "Button/_Secondary/Large/__Light/Loading",
+            "Button/_Secondary/Small/__Light/Hover  Active",
+            "Button/_Secondary/Large/__Light/Hover  Active",
+            "Button/_Secondary/Small/__Light/_Default",
+            "Button/_Secondary/Large/__Light/_Default",
+            "Button/__Primary/Small/__Light/Disabled",
+            "Button/__Primary/Large/__Light/Disabled",
+            "Button/__Primary/Small/__Light/Loading",
+            "Button/__Primary/Large/__Light/Loading",
+            "Button/__Primary/Small/__Light/Hover  Active",
+            "Button/__Primary/Large/__Light/Hover  Active",
+            "Button/__Primary/Small/__Light/_Default",
+            "Button/__Primary/Large/__Light/_Default",
+            "Button/_Sticky/01 Default",
+            "Button/_Sticky/03 Loading",
+            "Button/_Sticky/04 Disabled",
+            "Button/_Sticky/02 Hover + Active"
+        ]
+    },
+    {
+        componentName: "Card",
+        names: ["Card/__Light/_Base", "Card/_Dark/_Base"]
+    },
+    {
+        componentName: "CardDetails",
+        names: [
+            "Card/__Light/Detail/M-XL/_Default",
+            "Card/__Light/Detail/M-XL/With icon",
+            "Card/__Light/Detail/M-XL/With image",
+            "Card/_Dark/Detail/M-XL/_Default",
+            "Card/_Dark/Detail/M-XL/With icon",
+            "Card/_Dark/Detail/M-XL/With image",
+            "Card/__Light/Detail/XS-S/_Default",
+            "Card/__Light/Detail/XS-S/With icon",
+            "Card/__Light/Detail/XS-S/With image",
+            "Card/_Dark/Detail/XS-S/_Default",
+            "Card/_Dark/Detail/XS-S/With icon",
+            "Card/_Dark/Detail/XS-S/With image"
+        ]
     }
-]
+];
 
-export default symbols
+export default symbols;
